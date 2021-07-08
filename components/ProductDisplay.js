8f9008d75c5102e8c7f4f2c12db6cce7f22ffe63
Kitsada <game306202@gmail.com>
2021-07-09 02:57:07 +0700

    finalllllllllllllll

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -43,6 +43,8 @@ app.component ('product-display', {
                 </button>
             </div>
         </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
         </div>`,
         
         data() {
@@ -58,6 +60,7 @@ app.component ('product-display', {
                 ],
                 activeClass: true,
                 selectedVariant: 0,
+                reviews: [],
                 onSale: true,
     
             }
@@ -74,7 +77,11 @@ app.component ('product-display', {
             },
             updateVariant(index) {
                 this.selectedVariant = index;
+            },
+            addReview(review) {
+                this.reviews.push(review) 
             }
+            
         },
         computed: {
             title() {
